3c139d1e2fe3868ae6ca4471584c8f36740d3c26
f_kt6g <yujihi08@gmail.com>
2025-04-29 23:58:19 +0900

    写経

---
+++ Ruby/CherryBook/4/4.3.4_2.rb
@@ -0,0 +1,23 @@
+numbers = [1, 2 , 3 ,4]
+sum = 0
+numbers.each do |i|
+  sum += i
+end
+
+p sum
+
+numbers = [1, 2, 3, 4, 5]
+sum = 0
+numbers.each do |number|
+  sum += number
+end
+
+p sum
+
+numbers = [1, 2, 3, 4, 5, 6]
+sum = 0
+numbers.each do |element|
+  sum += element
+end
+
+p sum
