2833969b1db26720eccec5fbe41e4d38afdd5d5c
f_kt6g <yujihi08@gmail.com>
2025-05-08 23:26:42 +0900

    写経

---
+++ Ruby/CherryBook/2/2.11.2.rb
@@ -0,0 +1,22 @@
+''.empty?
+'abc'.empty?
+
+'watch'.include?('at')
+'watch'.include?('in')
+
+1.odd?
+2.odd?
+
+1.even?
+2.even?
+
+nil.nil?
+'abc'.nil?
+1.nil?
+
+def multiple_of_three?(n)
+  n % 3 == 0
+end
+multiple_of_three?(4)
+multiple_of_three?(5)
+multiple_of_three?(6)
